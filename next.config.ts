96028ddda9ec26b62e75181d353db1c694a5e830
Singularity Labs <ishandutta2007@gmail.com>
2026-01-19 07:53:36 +0530

    Add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/ai-researcher-roadmap-website",
+  assetPrefix: "/ai-researcher-roadmap-website/",
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
